248990a638dca06a20e792ebbd42222a782e7122
est-gonzalezr <74991415+est-gonzalezr@users.noreply.github.com>
2023-05-08 12:12:15 -0500

    Some filtering

--- exploratory-analysis/main.py
+++ exploratory-analysis/main.py
@@ -9,35 +9,48 @@ import os
 
 # Load the data
 df1 = pd.read_csv(os.path.join('data', 'Datos_proyecto_II_BI_2017.csv'), encoding= 'latin1')
+#print(len(df1.columns))
+#print(len(df1.index))
 df2 = pd.read_csv(os.path.join('data', 'Datos_proyecto_II_BI_2021.csv'), encoding= 'latin1')
+#print(len(df2.columns))
+#print(len(df2.index))
+df1 = df1.iloc[:, 1:]
+df2 = df2.iloc[:, 1:]
 
-#Join the dataframes as one on the same columns
-df = pd.concat([df1, df2], ignore_index=True)
+#print(df1)
 
+#ls = list(filter(lambda x: x not in df1.columns or x not in df2.columns, set(list(df1.columns) + list(df2.columns))))
+#print(len(ls))
 
-# Lets see the data
-print(df.head())
-print("--------------------")
+# 445 in both
+# 263 that are either in one or the other
+# 708 total
+# Combine the dataframes into one where the columns are the same
+df = pd.concat([df1, df2], axis=0, ignore_index=True)
 
-# Lets see the data types
-print(df.dtypes)
-print("--------------------")
+# # Lets see the data
+# print(df.head())
+# print("--------------------")
 
-# Lets see the data shape
-print(df.shape)
-print("--------------------")
+# # Lets see the data types
+# print(df.dtypes)
+# print("--------------------")
 
-# Lets see the data columns
-print(df.columns)
-print("--------------------")
+# # Lets see the data shape
+# print(df.shape)
+# print("--------------------")
 
-# Lets see the data info
-print(df.info())
-print("--------------------")
+# # Lets see the data columns
+# print(df.columns)
+# print("--------------------")
 
-# Lets see the data describe
-print(df.describe())
-print("--------------------")
+# # Lets see the data info
+# print(df.info())
+# print("--------------------")
 
-# Lets see the data null values
-print(df.isnull().sum())
+# # Lets see the data describe
+# print(df.describe())
+# print("--------------------")
+
+# # Lets see the data null values
+# print(df.isnull().sum())
